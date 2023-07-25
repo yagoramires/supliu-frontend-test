cbbc2cef605be73c3b42a4ba15af7b089904e035
yagoramires <yago.ramiresx@gmail.com>
2023-07-25 14:40:22 -0300

    feat: layout e configuracao da pagina Home e do componente Album

---
+++ src/components/Album.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+export default function Album({ data }) {
+  return (
+    <li>
+      <h2 className='font-bold mb-2'> Álbum: {`${data.name}, ${data.year}`}</h2>
+      <div className='flex items-center justify-center text-zinc-700 font-medium mb-1'>
+        <h3 className='w-[10%]'>Nº</h3>
+        <h3 className='flex-1'>Faixa</h3>
+        <h3 className='w-[10%]'>Duração</h3>
+      </div>
+
+      <ul>
+        {data.tracks.length > 0 ? (
+          data.tracks.map((track) => (
+            <li
+              key={track.id}
+              className='flex items-center justify-center text-zinc-700 font-medium mb-1'
+            >
+              <p className='w-[10%]'>{track.number}</p>
+              <p className='flex-1'>{track.title}</p>
+              <p className='w-[10%]'>{track.duration}</p>
+            </li>
+          ))
+        ) : (
+          <li>Nenhuma música cadastrada.</li>
+        )}
+      </ul>
+    </li>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllAlbums } from '../redux/actions';
+import Album from '../components/Album';
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -13,5 +14,28 @@ export default function Home() {
     dispatch(getAllAlbums());
   }, []);
 
-  return <div className='mainContainer'>Home</div>;
+  return (
+    <div
+      className={`bg-[url(/background.png)] bg-no-repeat bg-cover min-h-screen w-full flex justify-center items-center`}
+    >
+      <div className='w-[90%] max-w-[800px] bg-[rgba(255,255,255,0.7)]'>
+        <div className='bg-white flex justify-between items-center p-4'>
+          <img src='/logo.png' alt='' />
+          <h2 className='text-3xl text-zinc-500'>Discografia</h2>
+        </div>
+
+        <div>search</div>
+
+        {albums.length > 0 ? (
+          albums.map((album) => (
+            <ul key={album.id} className='p-4'>
+              <Album data={album} />
+            </ul>
+          ))
+        ) : (
+          <p>Nenhum álbum cadastrado.</p>
+        )}
+      </div>
+    </div>
+  );
 }
